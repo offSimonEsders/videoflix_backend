ae37cd261a7e5c62854d895bde8af731693426fc
simon <simonesders2003@gmail.com>
2024-02-11 21:00:35 +0100

    save converted videos in object and delete all videos of obeject

--- video/signals.py
+++ video/signals.py
@@ -1,18 +1,22 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-import os, shutil
+import os
 
 from video.models import Video
-from video.tasks import convert_video_to_720p
+from video.tasks import convert_video_to_720p, convert_video_to_480p
 
 
 @receiver(post_save, sender=Video)
 def save_video(sender, instance, created, **kwargs):
     if created:
-        convert_video_to_720p(instance.original_video.path)
+        instance.video_720p = convert_video_to_720p(instance.original_video.path)
+        instance.video_480p = convert_video_to_480p(instance.original_video.path)
+        instance.save()
 
 
 @receiver(post_delete, sender=Video)
 def delete_video(sender, instance, **kwargs):
     print(instance.original_video)
     os.remove(instance.original_video.path)
+    os.remove(instance.video_720p.path)
+    os.remove(instance.video_480p.path)
--- video/tasks.py
+++ video/tasks.py
@@ -4,6 +4,15 @@ import time
 
 
 def convert_video_to_720p(video_file_path):
+    new_path = video_file_path.replace('.mp4', '') + '_720p.mp4'
     cmd = 'ffmpeg -i "{}" -vf scale=1420:720 "{}"'.format(
-        video_file_path, video_file_path.replace('.mp4', '') + '_720p.mp4')
+        video_file_path, new_path)
     subprocess.run(cmd, capture_output=True)
+    return new_path
+
+def convert_video_to_480p(video_file_path):
+    new_path = video_file_path.replace('.mp4', '') + '_480p.mp4'
+    cmd = 'ffmpeg -i "{}" -vf scale=960:480 "{}"'.format(
+        video_file_path, new_path)
+    subprocess.run(cmd, capture_output=True)
+    return new_path
