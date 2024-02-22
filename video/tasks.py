09fcfb6a2408d7374e840d8205d2477c97f6505f
simon <simonesders2003@gmail.com>
2024-02-22 18:08:07 +0100

    test

--- video/tasks.py
+++ video/tasks.py
@@ -5,12 +5,12 @@ import time
 
 def convert_video_to_720p(video_file_path):
     new_path = video_file_path.replace('.mp4', '') + '_720p.mp4'
-    cmd = 'sudo ffmpeg -i "{}" -vf scale=1420:720 "{}"'.format(
+    cmd = 'sudo ffmpeg -i {} -vf scale=1420:720 {}'.format(
         video_file_path, new_path)
     subprocess.run(cmd, capture_output=True)
 
 def convert_video_to_480p(video_file_path):
     new_path = video_file_path.replace('.mp4', '') + '_480p.mp4'
-    cmd = 'sudo ffmpeg -i "{}" -vf scale=960:480 "{}"'.format(
+    cmd = 'sudo ffmpeg -i {} -vf scale=960:480 {}'.format(
         video_file_path, new_path)
     subprocess.run(cmd, capture_output=True)
