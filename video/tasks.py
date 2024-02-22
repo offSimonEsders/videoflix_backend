a3badf2b443fc4ce27535ddde5ac3f7f6b0fd742
simon <simonesders2003@gmail.com>
2024-02-22 18:22:53 +0100

    working video convert on linux

--- video/tasks.py
+++ video/tasks.py
@@ -13,8 +13,13 @@ def convert_video_to_720p(video_file_path):
     ]
     subprocess.run(cmd, capture_output=True)
 
+
 def convert_video_to_480p(video_file_path):
-    new_path = video_file_path.replace('.mp4', '') + '_480p.mp4'
-    cmd = 'sudo ffmpeg -i {} -vf scale=960:480 {}'.format(
-        video_file_path, new_path)
+    new_path = video_file_path.replace('.mp4', '_720p.mp4')
+    cmd = [
+        "ffmpeg", "-i",
+        video_file_path,
+        "-vf", "scale=960:480",
+        new_path
+    ]
     subprocess.run(cmd, capture_output=True)
