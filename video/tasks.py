5ec639d7b91ea870f64a927f1142742028054008
simon <simonesders2003@gmail.com>
2024-02-22 18:15:04 +0100

    right resolution

--- video/tasks.py
+++ video/tasks.py
@@ -5,7 +5,7 @@ import time
 
 def convert_video_to_720p(video_file_path):
     new_path = video_file_path.replace('.mp4', '') + '_720p.mp4'
-    cmd = 'sudo ffmpeg -i {} -vf scale=1420:720 {}'.format(
+    cmd = 'sudo ffmpeg -i {} -vf scale=1280:720 {}'.format(
         video_file_path, new_path)
     subprocess.run(cmd, capture_output=True)
 
