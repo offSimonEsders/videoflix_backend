4f35f8c66d19ea77f3e067c4720894cb28fce70d
offSimonEsders <simonesders2003@gmail.com>
2024-02-22 20:49:15 +0100

    postgresql

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'videoflix_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'videoflix_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345678#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
