60e70750dadcd39b99432579ff2e1f07d9416aa5
offSimonEsders <simonesders2003@gmail.com>
2024-03-12 11:35:19 +0100

    fixed creating verifycode

--- account/models.py
+++ account/models.py
@@ -3,16 +3,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.crypto import get_random_string
 
-
-# Create your models here.
-
 class VideoflixUser(AbstractUser):
     email = models.EmailField(max_length=254, default='', unique=True)
     verified = models.BooleanField(default=False)
-    verification_code = models.CharField(max_length=30, default=get_random_string(length=30), blank=True, unique=True)
+    verification_code = models.CharField(default='', blank=True, unique=False)
     reset_code = models.CharField(max_length=100, blank=True, null=True, unique=False)
     REQUIRED_FIELDS = ['email']
 
     def create_reset_code(self):
         self.reset_code = get_random_string(length=30) + self.email
         self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.verification_code:
+            self.verification_code = get_random_string(length=30) + self.email
+        super().save(*args, **kwargs)
